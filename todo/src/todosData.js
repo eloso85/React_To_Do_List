c781767b3643e50905d11416a784e07a2a54bcf0
unknown <eloso85@gmail.com>
2020-02-29 17:05:51 -0600

    adding data

---
+++ todo/src/todosData.js
@@ -0,0 +1,33 @@
+const todosData = [
+    {
+        id:1,
+        text:"Take out the trash",
+        completed: true
+    },
+
+    {
+        id:2 ,
+        text:"Grocery shopping" ,
+        completed: false
+    },
+
+    {
+        id: 3,
+        text:"Clean Gecko tank" ,
+        completed: false
+    },
+
+    {
+        id:4 ,
+        text:"Mow Lawn" ,
+        completed: true
+    },
+
+    {
+        id:5,
+        text:"Catch up on you" ,
+        completed:false
+    }
+
+]
+export default todosData
